0c61cfdac60a4a0477041bcb0200a00b9c34938c
zeann <czeannl21@gmail.com>
2024-06-04 11:50:03 +0700

    selesaii

---
+++ MC2_ScolioSense/XRay/CobbsLine.swift
@@ -0,0 +1,8 @@
+//
+//  CobbsLine.swift
+//  MC2_ScolioSense
+//
+//  Created by Jonathan Zeann Philibert Samuel on 04/06/24.
+//
+
+import Foundation
---
+++ MC2_ScolioSense/XRay/XRayScoliosis.swift
@@ -0,0 +1,18 @@
+//
+//  XRayScoliosis.swift
+//  MC2_ScolioSense
+//
+//  Created by Jonathan Zeann Philibert Samuel on 04/06/24.
+//
+
+import SwiftUI
+
+struct XRayScoliosis: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    XRayScoliosis()
+}
